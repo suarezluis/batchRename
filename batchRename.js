f58561b0c6ae8e9be8e08541c099001d88eb73dd
Luis Suarez <suarezluis@gmail.com>
2019-02-11 10:43:16 -0600

    Moving to programming

--- batchRename.js
+++ batchRename.js
@@ -7,7 +7,12 @@ const pdfMerger = require("pdf-merger-trvl");
 
 let filesToRename = [];
 
+let allFiles = [];
+
 console.clear();
+fs.readdir("./", (err, files) => {
+  allFiles = files;
+});
 console.log("       ╔════════════════════════╗");
 console.log("       ║                        ║");
 console.log("       ║  Road Vantage - Admin  ║");
@@ -35,74 +40,69 @@ inquirer
           message: "Please Enter Admin Initials"
         })
         .then(answer => {
-          fs.readdir(__dirname, (err, files) => {
-            console.log("   The following files will be affected;");
-            console.log();
-            files.forEach(file => {
-              if (file.slice(-6) === "_B.pdf" || file.slice(-7) === "_LB.pdf") {
-                filesToRename.push(file);
+          console.log("   The following files will be affected;");
+          console.log();
+          allFiles.forEach(file => {
+            if (file.slice(-6) === "_B.pdf" || file.slice(-7) === "_LB.pdf") {
+              filesToRename.push(file);
+
+              console.log(
+                "       " +
+                  file +
+                  " -> " +
+                  file.slice(0, file.length - 4) +
+                  "_" +
+                  answer.initials.toUpperCase() +
+                  file.slice(-4)
+              );
+            }
+          });
+          console.log();
+
+          inquirer
+            .prompt({
+              name: "option",
+              type: "list",
+              message: "Would you like to proceed?",
+              choices: [
+                "Yes proceed rename only",
+                "Yes proceed and add QC Sheet",
+                "No, quit."
+              ]
+            })
+            .then(answers => {
+              if (answers.option === "No, quit.") {
+                return;
+              }
+              if (answers.option === "Yes proceed rename only") {
+                exec(`ren *_B.pdf *_B_${answer.initials.toUpperCase()}.pdf`);
+                exec(`ren *_LB.pdf *_LB_${answer.initials.toUpperCase()}.pdf`);
 
                 console.log(
-                  "       " +
-                    file +
-                    " -> " +
-                    file.slice(0, file.length - 4) +
-                    "_" +
-                    answer.initials.toUpperCase() +
-                    file.slice(-4)
+                  "Process finished succesfully! Please revise your folder."
                 );
+                exec("pause");
+              }
+
+              if (answers.option === "Yes proceed and add QC Sheet") {
+                filesToRename.forEach(file => {
+                  let merger = new pdfMerger();
+
+                  merger.add("./QC.pdf"); //merge all pages. parameter is the path to file and filename.
+                  console.log(`Adding :${file}|`);
+                  merger.add("./" + file); // merge only page 2
+                  console.log(`Merging :${file}|`);
+                  merger.save(file); //save under given name
+                });
+
+                console.log(
+                  "Process finished succesfully! Please revise your folder."
+                );
+                exec(`ren *_B.pdf *_B_${answer.initials.toUpperCase()}.pdf`);
+                exec(`ren *_LB.pdf *_LB_${answer.initials.toUpperCase()}.pdf`);
+                exec("pause");
               }
             });
-            console.log();
-            inquirer
-              .prompt({
-                name: "option",
-                type: "list",
-                message: "Would you like to proceed?",
-                choices: [
-                  "Yes proceed rename only",
-                  "Yes proceed and add QC Sheet",
-                  "No, quit."
-                ]
-              })
-              .then(answers => {
-                if (answers.option === "No, quit.") {
-                  return;
-                }
-                if (answers.option === "Yes proceed rename only") {
-                  exec(`ren *_B.pdf *_B_${answer.initials.toUpperCase()}.pdf`);
-                  exec(
-                    `ren *_LB.pdf *_LB_${answer.initials.toUpperCase()}.pdf`
-                  );
-
-                  console.log(
-                    "Process finished succesfully! Please revise your folder."
-                  );
-                  exec("pause");
-                }
-
-                if (answers.option === "Yes proceed and add QC Sheet") {
-                  filesToRename.forEach(file => {
-                    var merger = new pdfMerger();
-
-                    merger.add("./QC.pdf"); //merge all pages. parameter is the path to file and filename.
-                    merger.add("./" + file); // merge only page 2
-
-                    merger.save(file); //save under given name
-                  });
-
-                  exec(`ren *_B.pdf *_B_${answer.initials.toUpperCase()}.pdf`);
-                  exec(
-                    `ren *_LB.pdf *_LB_${answer.initials.toUpperCase()}.pdf`
-                  );
-
-                  console.log(
-                    "Process finished succesfully! Please revise your folder."
-                  );
-                  exec("pause");
-                }
-              });
-          });
         });
     }
   });
